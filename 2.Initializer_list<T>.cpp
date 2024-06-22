bdefae2dac922c1e7d5261dd39c239627de817dd
Dor04ka <145880294+Dor04ka@users.noreply.github.com>
2024-06-22 20:28:38 +0300

    Update 2.Initializer_list<T>.cpp

--- 2.Initializer_list<T>.cpp
+++ 2.Initializer_list<T>.cpp
@@ -1,1 +1,38 @@
+//№2. Initializer_list<T>
 
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <initializer_list>
+using namespace std;
+
+// функция, аргументами которой служит список значений в фигурных скобках
+void printValues(std::initializer_list<int> values) {
+	for (auto value : values) {
+		std::cout << value << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+
+	cout << "№2. Initializer_list<T>" << endl;
+	// В данном примере мы рассмотрим вектор, который будем заполнять без push_back()
+	vector<string> musicGenres;
+	musicGenres.reserve(7);
+	// заполним вектор 
+	musicGenres = { "pop", "metal", "rap", "jazz", "falk", "blues", "phonk" };
+	// используем итератор для вывода элементов
+	for (auto it = musicGenres.begin(); it != musicGenres.end(); ++it) {
+		std::cout << *it << " ";
+	}
+
+	cout << endl; 
+
+	// с вектором закончили. Теперь рассмотрим еще один пример, на этот раз - с функцией
+	// функция принимает initializer_list<int>, что позволяет передавать список значений в фигурных скобках напрямую в качестве аргумента
+	printValues({ 1, 2, 3, 4, 5 }); 
+}
